c00a356415dcac5c428d6769344c4b1ce3467a1e
MohamedH1998 <mohamedahassan188@gmail.com>
2022-02-04 13:45:30 +0000

    Add deps

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
-  "parser": "babel-eslint",
+  parser: "babel-eslint",
   root: true,
   env: {
     es6: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
+  extends: ["eslint:recommended", "google"],
   rules: {
     quotes: ["error", "double"],
   },
+  plugins: ["only-warn"],
 };
